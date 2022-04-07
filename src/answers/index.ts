d3fc067a902683b479daa5081a5c76145d2f37cf
Bill Chen (Mac) <ybchen@umich.edu>
2022-04-07 11:17:34 -0400

    build

--- src/answers/index.ts
+++ src/answers/index.ts
@@ -2,7 +2,11 @@ import { getHint } from '../logic'
 import { answers } from './list'
 
 export function getAnswerOfDay(day: number) {
-  const [word = '', hint = ''] = answers[day] || []
+/**
+ *  const [word = '', hint = ''] = answers[day] || []
+ */
+  const word = '小猪佩奇'
+  const hint = '猪'
   return {
     word,
     hint: hint || getHint(word),
